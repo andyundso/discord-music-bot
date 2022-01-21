db69af8f6a6de925b5a72d03f19f57e182111872
Andy Pfister <andy.pfister@hotmail.ch>
2022-01-21 19:10:09 +0100

    Reformat project with ESLint

--- src/commands/findSoundcloud.ts
+++ src/commands/findSoundcloud.ts
@@ -1,31 +1,34 @@
-import {Message} from "discord.js";
+import { Message } from 'discord.js';
+import { checkPrerequisite, sendToQueue } from './commons';
+import { QueueConstruct } from '../interfaces';
+
 const scdl = require('soundcloud-downloader').default;
-import {QueueConstruct} from "../index";
-import {checkPrerequisite, sendToQueue} from "./commons";
 
-export async function findSoundcloud(message: Message, serverQueue: QueueConstruct | undefined) {
-    if (!(await checkPrerequisite(message))) {
-        return;
-    }
+async function findSoundcloud(message: Message, serverQueue: QueueConstruct | undefined) {
+  if (!(await checkPrerequisite(message))) {
+    return;
+  }
 
-    const args = message.content.split(" ");
-    const voiceChannel = message.member!.voice.channel!;
+  const args = message.content.split(' ');
+  const voiceChannel = message.member!.voice.channel!;
 
-    let songInfo;
-    try {
-        songInfo = await scdl.getInfo(args[1])
-        console.log(songInfo)
-    } catch (error) {
-        console.error(error)
-        await message.channel.send('sorry, da hed was ned klapped :(')
-        return;
-    }
+  let songInfo;
+  try {
+    songInfo = await scdl.getInfo(args[1]);
+    console.log(songInfo);
+  } catch (error) {
+    console.error(error);
+    await message.channel.send('sorry, da hed was ned klapped :(');
+    return;
+  }
 
-    const song = {
-        source: "soundcloud",
-        title: songInfo.title!,
-        url: args[1],
-    };
+  const song = {
+    source: 'soundcloud',
+    title: songInfo.title!,
+    url: args[1],
+  };
 
-    sendToQueue(serverQueue, message, voiceChannel, song);
+  sendToQueue(serverQueue, message, voiceChannel, song);
 }
+
+export default findSoundcloud;
---
+++ src/commands/playBanger.ts
@@ -0,0 +1,35 @@
+import { Message } from 'discord.js';
+import { QueueConstruct } from '../interfaces';
+import findYouTube from './findYoutube';
+
+const remastered = [
+  'https://www.youtube.com/watch?v=DxHDxB2mVg4', // HRR
+  'https://www.youtube.com/watch?v=YaASkhAwkrY', // Chill
+  'https://www.youtube.com/watch?v=QlWMQI3kQ7w', // brasilianischi Flow
+  'https://www.youtube.com/watch?v=6YuitpJ6zvk', // divinorum bay rundi
+  'https://www.youtube.com/watch?v=fovqQn8ZF6U', // quoli drbt3
+  'https://www.youtube.com/watch?v=f72OYEvKw0U', // sven chiller
+  'https://www.youtube.com/watch?v=ycMbnaOYJsg', // ich los dir zue
+  'https://www.youtube.com/watch?v=ADuI5Jtgp7Y', // mini stund
+  'https://www.youtube.com/watch?v=Xl9MAaxNfok', // gangster radio
+  'https://www.youtube.com/watch?v=Te_3GNJXRI8', // peaky blinder
+  'https://www.youtube.com/watch?v=mP7QWjJCIw8', // da baby zh
+  'https://www.youtube.com/watch?v=7A--pKIUhGg', // erwache
+  'https://www.youtube.com/watch?v=ZFUVgDhtyzw', // dope deala assassin
+  'https://www.youtube.com/watch?v=rGYGqXnVW9w', // KUSH
+  'https://www.youtube.com/watch?v=tgPTUSeSEBA', // Paffee
+  'https://www.youtube.com/watch?v=OcRynMc8lLc', // divinorum 6tel
+  'https://www.youtube.com/watch?v=tbH9mXQipDM', // kollegah drive by
+  'https://www.youtube.com/watch?v=o875br8N4Fs', // pitbull hotel room service
+  'https://www.youtube.com/watch?v=PP0uRgsQtJk', // pitbull dont stop the party
+  'https://www.youtube.com/watch?v=iP9FfkcaTLw', // pitbull i know you want me
+  'https://www.youtube.com/watch?v=KvMoU38_zy0', // polish cow
+];
+
+function playBanger(message: Message, serverQueue: QueueConstruct | undefined) {
+  const commandMessage = { ...message } as Message;
+  commandMessage.content = `!play ${remastered[Math.floor(Math.random() * remastered.length)]}`;
+  findYouTube(commandMessage, serverQueue);
+}
+
+export default playBanger;
